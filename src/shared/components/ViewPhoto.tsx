58a94eb71d11f6aea0985748b5048d6c4c3cc0a8
THN91 <taras.gonchahruk22@gmail.com>
2023-01-31 00:56:37 +0200

    Finish view photo page

--- src/shared/components/ViewPhoto.tsx
+++ src/shared/components/ViewPhoto.tsx
@@ -1,36 +1,67 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {useNavigate} from 'react-router-dom';
 
-import {AppBar, Box, IconButton, ImageListItem, Stack, Toolbar} from "@mui/material";
-import AddIcon from "@mui/icons-material/Add";
-import StarBorderOutlinedIcon from "@mui/icons-material/StarBorderOutlined";
-import FileUploadOutlinedIcon from "@mui/icons-material/FileUploadOutlined";
-import DeleteOutlineOutlinedIcon from "@mui/icons-material/DeleteOutlineOutlined";
+import {AppBar, Box, IconButton, ImageListItem, Stack, styled, Toolbar} from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
+import StarBorderOutlinedIcon from '@mui/icons-material/StarBorderOutlined';
+import FileUploadOutlinedIcon from '@mui/icons-material/FileUploadOutlined';
+import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
-import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import StarOutlinedIcon from '@mui/icons-material/StarOutlined';
 
-import {colors} from "../../styles/variables";
-import {useAppDispatch, useAppSelector} from "../hooks/redux_hooks";
-import {getPhoto} from "../store/photoSlice";
+import {colors} from '../../styles/variables';
+import {useAppDispatch, useAppSelector} from '../hooks/redux_hooks';
+import {IPhoto} from '../interfaces';
+import {updatePhoto} from '../store/photoSlice';
+
+const PrevNextBtn = styled(IconButton)(() => ({
+  width: 64,
+  height: 64,
+  backgroundColor: colors.light.bgBtnViewPhoto,
+  ['&:hover']: {
+    backgroundColor: colors.light.bgBtnViewPhoto,
+  },
+}));
 
 
 const ViewPhoto = (): JSX.Element => {
   const navigate = useNavigate();
   const dispatch = useAppDispatch();
-  const {photos} = useAppSelector(state => state.photo);
+  const {photos, viewPhoto} = useAppSelector(state => state.photo);
+  const [currentPhoto, setCurrentPhoto] = useState<number>();
+  const indexPhoto = photos.findIndex(item => item.id === viewPhoto);
+
 
   useEffect(() => {
-    dispatch(getPhoto([4]))
-  }, [])
+    setCurrentPhoto(indexPhoto);
+  }, []);
+
+  const nextPhoto = () => setCurrentPhoto(Number(currentPhoto) + 1);
+
+  const prevPhoto = () => setCurrentPhoto(Number(currentPhoto) - 1);
+
+  const addPhotoFavorites = () => {
+    const photo = photos.find(item => item.id === photos[Number(currentPhoto)].id) as Required<IPhoto>;
+    dispatch(updatePhoto({...photo, isFavorite: !photo.isFavorite}));
+  };
 
   return (
     <Box sx={{
       height: '100%',
       backgroundColor: '#000000',
-      color: colors.light.colorIcon
-    }}>
-      <AppBar position="absolute" color="transparent" sx={{boxShadow: 'none'}}>
+      color: colors.light.colorIcon,
+    }}
+    >
+      <AppBar
+        position="absolute"
+        color="transparent"
+        sx={{
+          boxShadow: 'none',
+          background: 'linear-gradient(180deg, rgba(0, 0, 0, 0.2) 0%, rgba(0, 0, 0, 0) 100%)',
+        }}
+      >
         <Toolbar sx={{margin: '0 40px', justifyContent: 'space-between', alignItems: 'center'}}>
           <IconButton
             onClick={() => navigate(-1)}
@@ -43,8 +74,11 @@ const ViewPhoto = (): JSX.Element => {
             <IconButton size="large" color="inherit">
               <AddIcon/>
             </IconButton>
-            <IconButton size="large" color="inherit">
-              <StarBorderOutlinedIcon/>
+            <IconButton onClick={() => addPhotoFavorites()} size="large" color="inherit">
+              {photos[Number(currentPhoto)] && photos[Number(currentPhoto)].isFavorite ?
+                <StarOutlinedIcon/> :
+                <StarBorderOutlinedIcon/>
+              }
             </IconButton>
             <IconButton size="large" color="inherit">
               <FileUploadOutlinedIcon/>
@@ -55,24 +89,30 @@ const ViewPhoto = (): JSX.Element => {
           </Stack>
         </Toolbar>
       </AppBar>
-      <Stack spacing={8} direction="row" sx={{justifyContent: 'center', alignItems: 'center'}}>
-        <IconButton size="large" color="inherit">
-          <ArrowBackIosIcon/>
-        </IconButton>
-        {photos[0] &&
+      <Stack direction="row" sx={{justifyContent: 'center', alignItems: 'center'}}>
+        <Box sx={{display: 'flex', justifyContent: 'center', width: '20vw'}}>
+          {currentPhoto !== 0 &&
+            <PrevNextBtn onClick={() => prevPhoto()} color="inherit">
+              <ArrowBackIosIcon/>
+            </PrevNextBtn>}
+        </Box>
+        {currentPhoto !== undefined &&
           <ImageListItem>
             <img
               style={{width: '80vw', height: '100vh'}}
-              src={`data:${photos[0].type};base64,${photos[0].image}`}
-              alt={photos[0].description}/>
+              src={`data:${photos[currentPhoto].type};base64,${photos[currentPhoto].image}`}
+              alt={photos[currentPhoto].description}/>
           </ImageListItem>
         }
-        <IconButton size="large"  color="inherit">
-          <ArrowForwardIosIcon/>
-        </IconButton>
+        <Box sx={{display: 'flex', justifyContent: 'center', width: '20vw'}}>
+          {photos.length - 1 !== currentPhoto &&
+            <PrevNextBtn onClick={() => nextPhoto()} sx={{}} color="inherit">
+              <ArrowForwardIosIcon/>
+            </PrevNextBtn>}
+        </Box>
       </Stack>
     </Box>
-  )
+  );
 };
 
 export default ViewPhoto;
