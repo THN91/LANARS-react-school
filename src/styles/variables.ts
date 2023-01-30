05662202f4eb094adf0b2543592836bee1275840
THN91 <taras.gonchahruk22@gmail.com>
2023-01-31 00:50:18 +0200

    Add color

--- src/styles/variables.ts
+++ src/styles/variables.ts
@@ -2,6 +2,7 @@ export const colors = {
   light: {
     primary: '#1786ED',
     text: '#3F4A53',
+    bgBtnViewPhoto: '#3F4A53',
     colorIcon: '#E5EDF2',
     checkbox: '#fff',
     divider: '#E5EDF2',
